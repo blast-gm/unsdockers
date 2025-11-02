84ec3f587da569e4a46f0c3824eab0ae78369c63
blast-gm <francisco.vaylton@gmail.com>
2025-11-02 13:36:04 -0300

    Update entrypoint.sh

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -1,11 +1,27 @@
-#!/bin/bash
-sleep 1
+#!/bin/ash
 
-cd /home/container
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+export INTERNAL_IP
 
-# Run the Server
-${MODIFIED_STARTUP}
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Print PHP version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mphp --version\n"
+php --version
+
+# Convert all of the "{{VARIABLE}}" parts of the command into the expected shell
+# variable format of "${VARIABLE}" before evaluating the string and automatically
+# replacing the values.
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Display the command we're running in the output, and then execute it with the env
+# from the container itself.
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# shellcheck disable=SC2086
+exec env ${PARSED}
